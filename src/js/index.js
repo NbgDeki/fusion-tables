4c635b213f6f6493256dba6b835f9c988391a948
nesacar <nebojsart1409@yahoo.com>
2018-02-22 17:00:47 +0100

    product page

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,21 @@ import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel';
 
 $(function () {
-    $('.owl-carousel').owlCarousel({
+    $('.home-slider').owlCarousel({
         items: 1,
         autoHeight: true,
         nav: true,
         navText: ['<img src="https://ministudio.rs/download/arrow-left.png">', '<img src="https://ministudio.rs/download/arrow-right.png">'],
+        loop: true,
+        autoplay: true,
+        smartSpeed: 700,
+    });
+
+    $('.product-slider').owlCarousel({
+        items: 1,
+        autoHeight: true,
+        nav: true,
+        navText: ['<img src="https://ministudio.rs/download/blue-left-arrow.jpg">', '<img src="https://ministudio.rs/download/blue-right-arrow.jpg">'],
         loop: true,
         autoplay: true,
         smartSpeed: 700,
