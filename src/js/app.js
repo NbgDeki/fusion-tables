3cd177e1c1d8e828205b6bfb4426a3b4acae5395
nbgdeki <radic.dejan.nbg@gmail.com>
2018-03-01 16:20:02 +0100

    deki

--- src/js/app.js
+++ src/js/app.js
@@ -44,4 +44,29 @@ $(function () {
         $('#desktop-side-menu').toggleClass('active');
         $('.two-boxes').toggleClass('active');
     });
+
+
+    //back to top button 
+    if ($('#back-to-top').length) {
+        var scrollTrigger = 20, // px
+            backToTop = function () {
+                var scrollTop = $(window).scrollTop();
+                if (scrollTop > scrollTrigger) {
+                    $('#back-to-top').addClass('show');
+                } else {
+                    $('#back-to-top').removeClass('show');
+                }
+            };
+        backToTop();
+        $(window).on('scroll', function () {
+            backToTop();
+        });
+        $('#back-to-top').on('click', function (e) {
+            e.preventDefault();
+            $('html,body').animate({
+                scrollTop: 0
+            }, 700);
+        });
+    }
+
 });
