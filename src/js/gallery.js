3f722d7e31357aca5de0bccdb33e4809c8f97436
VacaSan <vacasan@DESKTOP-9EEBETM.localdomain>
2018-03-05 17:00:16 +0100

    gallery displays selected image

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,12 +1,33 @@
 $(document).ready(function () {
-    $('#jsImageGallery').owlCarousel({
-        autoWidth: true,
+    // cache dom
+    const $imageGallery = $('#jsImageGallery'),
+        $imageBox = $('#jsImageBox');
+
+    // bind events
+    $imageGallery.on('click', _handleThumbnailClicks);
+
+    // inti carousel
+    // TODO Add responsive brakepoints
+    $imageGallery.owlCarousel({
+        // autoWidth: true,
+        items: 5,
         nav: true,
-        rewind: true,
+        // rewind: true,
         navText: [
             '<img class="carousel-controls carousel-controls--left" src="https://www.fusiontables.rs/img/blue-left-arrow.jpg">', 
             '<img class="carousel-controls carousel-controls--right" src="https://www.fusiontables.rs/img/blue-right-arrow.jpg">'
         ],
         margin: 10
     });
+
+    // click handler
+    function _handleThumbnailClicks (evt) {
+        let src,
+            $target = $(evt.target);
+
+        if (!$target.is('[data-is-thumbnail]')) return;
+
+        src = $target.attr('src');
+        $imageBox.attr('src', src);
+    }
 });
