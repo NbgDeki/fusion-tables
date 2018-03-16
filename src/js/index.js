1d29ea4f90e189ed980787b56ed93848cea45b1c
VacaSan <kotaracvladan@gmail.com>
2018-03-16 11:22:07 +0100

    move slider to top of product page. remove repetitive js

--- src/js/index.js
+++ src/js/index.js
@@ -2,46 +2,34 @@ import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel';
 
 $(function () {
-    $('.home-slider').owlCarousel({
-        items: 1,
-        autoHeight: true,
-        dots: true,
-        nav: true,
-        navText: [
-          '<img src="https://ministudio.rs/download/srelica-levo.png">',
-          '<img src="https://ministudio.rs/download/strelica-desno.png">'
-        ],
-        loop: true,
-        autoplay: true,
-        smartSpeed: 700,
-    });
+  const carousels = [
+    {
+      name: '.home-slider',
+      options: { dots: true }
+    },
+    {
+      name: '.product-slider',
+      options: {}
+    }
+  ];
 
-    var owl = $('.product-slider').owlCarousel({
-        items: 1,
-        autoHeight: true,
-        nav: true,
-        navText: [
-          '<img src="https://www.fusiontables.rs/img/blue-left-arrow.jpg" class="blue-arrow blue-arrow--left">',
-          '<img src="https://www.fusiontables.rs/img/blue-right-arrow.jpg" class="blue-arrow blue-arrow--right">'
-        ],
-        loop: true,
-        autoplay: true,
-        smartSpeed: 700,
-    });
+  const defaultOptions = {
+    items: 1,
+    autoHeight: true,
+    dots: false,
+    nav: true,
+    navText: [
+      '<img class="ft-arrow ft-arrow--left" src="https://ministudio.rs/download/srelica-levo.png">',
+      '<img class="ft-arrow ft-arrow--right" src="https://ministudio.rs/download/strelica-desno.png">'
+    ],
+    loop: true,
+    autoplay: true,
+    smartSpeed: 700
+  };
 
-    $(".blue-left-arrow").click(function () {
-        owl.trigger('prev.owl.carousel');
-    });
-
-    $(".blue-right-arrow").click(function () {
-        owl.trigger('next.owl.carousel');
-    });
-        
-    $('.slajder').mouseover(function(){
-        $(this).find('.owl-nav').fadeIn(500);
-    });
-    $('.slajder').mouseleave(function(){
-        $(this).find('.owl-nav').fadeOut(500);
-    });
+  carousels.forEach(carousel => {
+    let options = Object.assign({}, defaultOptions, carousel.options);
+    $(carousel.name).owlCarousel(options);
+  });
 });
 
